2f193d288d55a2fcebe4e0b79dbe2a9c20330cc4
Angelina97 <angelina.grigorova.18@gmail.com>
2025-05-26 16:54:31 +0200

    upd

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -7,13 +7,13 @@ export async function fetchImages(query, page = 1, perPage = 15) {
 	try {
 		const response = await axios.get(BASE_URL, {
 			params: {
-				key: API_KEY,
-				q: query,
-				image_type: 'photo',
-				orientation: 'horizontal',
-				safesearch: true,
-				page: page,
-				per_page: perPage,
+			key: API_KEY,
+			q: query,
+			image_type: 'photo',
+			orientation: 'horizontal',
+			safesearch: true,
+			page: page,
+			per_page: perPage,
 			},
 		});
 		return response.data;
